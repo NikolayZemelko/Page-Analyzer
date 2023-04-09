5e5f60000cdd1bf535af9f4b26ae0ad574b63d30
NikolayZemelko <zemelko1989@gmail.com>
2023-04-09 13:22:14 +0300

    feat: add app.py

---
+++ page_analyzer/app.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask, request, render_template
+from dotenv import load_dotenv
+
+
+load_dotenv()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+
+@app.route('/')
+def index():
+    return render_template(
+        'index.html'
+    )
