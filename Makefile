3f9ff97fb71c523d3184441048fe0a9f05316d87
NikolayZemelko <zemelko1989@gmail.com>
2023-04-25 20:07:07 +0300

    fix: Change makefile, change database.sql file

--- Makefile
+++ Makefile
@@ -17,8 +17,5 @@ dev:
 
 PORT ?= 8000
 start:
-	sudo -u postgres createuser --createdb $(myname)
-	sudo -u postgres createdb --owner=$(myname) pageAnalyzerTest
-	createdb pageAnalyzerTest
-	sudo -u postgres psql -c "ALTER USER postgres PASSWORD '!Fromakorise1977';"
+	poetry run psql -U postgres -d pageAnalyzerTest < database.sql
 	poetry run gunicorn -w 5 -b 0.0.0.0:$(PORT) page_analyzer.app:app
--- database.sql
+++ database.sql
@@ -1,12 +1,7 @@
+DROP TABLE IF EXISTS urls;
+
 CREATE TABLE urls (
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name varchar(255),
     created_at timestamp
 );
-
-INSERT INTO urls (name, created_at) VALUES ('https://www.vk.com', '2023-04-24T18:11:44.443611'::timestamp);
-INSERT INTO urls (name, created_at) VALUES ('https://www.warcraft.com', '2023-04-24T18:29:23.792500'::timestamp);
-INSERT INTO urls (name, created_at) VALUES ('https://www.vk8.com', '2023-04-24T19:35:47.248273'::timestamp);
-INSERT INTO urls (name, created_at) VALUES ('https://www.dota2.com', '2023-04-24T21:11:07.508025'::timestamp);
-INSERT INTO urls (name, created_at) VALUES ('https://www.dota3.com', '2023-04-24T21:20:56.288827'::timestamp);
-INSERT INTO urls (name, created_at) VALUES ('https://io.hexlet.ru', '2023-04-24T21:21:45.963363'::timestamp);
