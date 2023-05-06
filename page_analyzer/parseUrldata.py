fd9f53211e8473f83f602e8903d8a41ad7e273ac
NikolayZemelko <zemelko1989@gmail.com>
2023-05-06 19:34:46 +0300

    fix: E501 flake8 error fixed

--- page_analyzer/parseUrldata.py
+++ page_analyzer/parseUrldata.py
@@ -7,7 +7,8 @@ def parse_url_data(url):
     soup = BeautifulSoup(requests.get(url).text, 'html.parser')
     h1 = '' if soup.h1 is None else soup.h1.get_text()
     title = '' if soup.title is None else soup.title.get_text()
-    content_raw = soup.find("meta", recursive=True, attrs={'name': 'description'})
+    content_raw = soup.find("meta", recursive=True,
+                            attrs={'name': 'description'})
     content = '' if content_raw is None else content_raw['content']
 
     return {
